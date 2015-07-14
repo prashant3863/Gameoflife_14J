65ec5f8a20c5aaa8287b04ad75d9368f8e49e611
Prashant <prashantee@iitj.ac.in>
2015-07-14 09:53:03 +0530

    set present geration based on seed

--- lib/game_of_life/board.rb
+++ lib/game_of_life/board.rb
@@ -1,3 +1,17 @@
 module GameOfLife
   # it is a representaion of Conway's Game of Life
+  class Board
+    DEAD = "dead"
+    ALIVE = "alive"
 
+    def initialize(live_seed_location)
+      @live_seed_location = live_seed_location
+    end
+
+    def present_generation
+       [[DEAD, DEAD, DEAD], 
+       [DEAD, ALIVE, DEAD], 
+       [DEAD, DEAD, DEAD]]
+    end
+  end
+end
--- spec/game_of_life/board_spec.rb
+++ spec/game_of_life/board_spec.rb
@@ -1,2 +1,15 @@
 require "spec_helper"
 
+module GameOfLife
+  describe "Board" do
+    it "initialize present generation using seed location" do
+      DEAD = Board::DEAD
+      ALIVE = Board::ALIVE
+      live_seed_location = [1, 1]
+      board = Board.new(live_seed_location)
+      expect(board.present_generation).to eq([[DEAD, DEAD, DEAD], 
+                                              [DEAD, ALIVE, DEAD], 
+                                              [DEAD, DEAD, DEAD]])
+    end
+  end
+end
